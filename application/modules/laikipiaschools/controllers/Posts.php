922cc9e8f230d057155ae3e0ea8385ee4e4807f9
samuel ndembu <samuelndembu5@gmail.com>
2019-03-06 15:12:40 +0300

    changes to be saved

--- application/modules/laikipiaschools/controllers/Posts.php
+++ application/modules/laikipiaschools/controllers/Posts.php
@@ -35,7 +35,7 @@ class posts extends MX_Controller
         }
         $this->form_validation->set_rules("post_title", "Post Title", "required");
         $this->form_validation->set_rules("post_description", "Post Description", "required");
-$this->form_validation->set_rules("category_id", "Post Category", "required");
+// $this->form_validation->set_rules("category_id", "Post Category", "required");
 $this->form_validation->set_rules("post_date", "Post Date", "required");
 
 
