49768c625de2f6af3c25b7f9f60311a00bbc8dbf
samuel ndembu <samuelndembu5@gmail.com>
2019-03-07 17:27:12 +0300

    changes to be saved

--- application/modules/laikipiaschools/views/Posts/all_posts.php
+++ application/modules/laikipiaschools/views/Posts/all_posts.php
@@ -231,7 +231,6 @@ if ($query->num_rows() > 0) {
         ?>
                                                     </p>
                                                 </div>
-
                                                 <div>
                                                     <?php if ($row->post_status == 1) {?>
                                                     <span class="badge badge-pill badge-success">Active</span>
