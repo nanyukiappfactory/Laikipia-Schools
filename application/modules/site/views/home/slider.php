58dcf056033bf298912272cc25116f41b180c061
samuel ndembu <samuelndembu5@gmail.com>
2019-03-11 15:07:22 +0300

    schools and slider.php updated

--- application/modules/site/views/home/slider.php
+++ application/modules/site/views/home/slider.php
@@ -20,7 +20,6 @@ foreach ($abouts->result() as $key => $row) {
         <?php }}?>
 
     </div><!-- End Carousel Inner -->
-
     <ul class="nav nav-pills nav-fill">
         <li class="nav-item" data-target="#myCarousel" data-slide-to="0">
             <a class="nav-link" href="#">
@@ -29,11 +28,17 @@ foreach ($abouts->result() as $key => $row) {
                         <div class="fc-icon">
                             <i class="fas fa-info"></i>
                         </div>
+                        <h5>About</h5>
+                        <h3>About the initiative</h3>
+                        <button type="submit" class="btn btn-default btn-dark">
+                            More
+                        </button>
+                    </div>
                 </figure>
             </a>
         </li>
         <li class="nav-item" data-target="" data-slide-to="1">
-            <a class="nav-link" href="<?php echo base_url(); ?>client/schools">
+            <a class="nav-link" href="<?php echo base_url(); ?> client/schools">
                 <figure class="slider-figure">
                     <div class="caption">
                         <div class="fc-icon">
@@ -41,52 +46,45 @@ foreach ($abouts->result() as $key => $row) {
                         </div>
                         <h5>Schools</h5>
                         <h3>Laikipia schools</h3>
-
                         <button type="submit" class="btn btn-default btn-dark">
                             More
                         </button>
                     </div>
-                    <h5>Schools</h5>
-                    <h3>Laikipia schools</h3>
-                    <button type="submit" class="btn btn-default btn-dark">
-                        More
-                    </button>
-</div>
-</figure>
-</a>
-</li>
-<li class="nav-item" data-target="#myCarousel" data-slide-to="2">
-    <a class="nav-link" href="#">
-        <figure class="slider-figure">
-            <div class="caption">
-                <div class="fc-icon">
-                    <i class="fas fa-hand-holding-usd"></i>
-                </div>
-                <h5>Donations</h5>
-                <h3>Promote education</h3>
-                <button type="submit" class="btn btn-default btn-dark">
-                    More
-                </button>
-            </div>
-        </figure>
-    </a>
-</li>
-<li class="nav-item" data-target="#myCarousel" data-slide-to="3">
-    <a class="nav-link" href="#">
-        <figure class="slider-figure">
-            <div class="caption">
-                <div class="fc-icon">
-                    <i class="fas fa-handshake"></i>
-                </div>
-                <h5>Partners</h5>
-                <h3>Our partners</h3>
-                <button type="submit" class="btn btn-default btn-dark">
-                    More
-                </button>
-            </div>
-        </figure>
-    </a>
-</li>
-</ul>
+                </figure>
+            </a>
+        </li>
+        <li class="nav-item" data-target="#myCarousel" data-slide-to="2">
+            <a class="nav-link" href="#">
+                <figure class="slider-figure">
+                    <div class="caption">
+                        <div class="fc-icon">
+                            <i class="fas fa-hand-holding-usd"></i>
+                        </div>
+                        <h5>Donations</h5>
+                        <h3>Promote education</h3>
+                        <button type="submit" class="btn btn-default btn-dark">
+                            More
+                        </button>
+                    </div>
+                </figure>
+            </a>
+        </li>
+        <li class="nav-item" data-target="#myCarousel" data-slide-to="3">
+            <a class="nav-link" href="#">
+                <figure class="slider-figure">
+                    <div class="caption">
+                        <div class="fc-icon">
+                            <i class="fas fa-handshake"></i>
+                        </div>
+                        <h5>Partners</h5>
+                        <h3>Our partners</h3>
+                        <button type="submit" class="btn btn-default btn-dark">
+                            More
+                        </button>
+                    </div>
+                </figure>
+            </a>
+        </li>
+    </ul>
 </div>
 <!-- End Carousel -->
