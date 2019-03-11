37a7851ee5bee084c8c35977b77d09651cb0fb94
Muraura <patricia.kanana@nanyukiappfactory.onmicrosoft.com>
2019-03-11 15:31:04 +0300

    school link working

--- application/modules/site/controllers/Site.php
+++ application/modules/site/controllers/Site.php
@@ -45,7 +45,6 @@ class Site extends MX_Controller
     {
         
             $v_data['abouts'] = $this->sites_model->get_about_posts();
-            
             $v_data['get_donors'] = $this->sites_model->get_donations();
             $v_data['get_dignity_packs'] = $this->sites_model->get_donations();
             $donations = $this->sites_model->get_donation_totals();
@@ -73,42 +72,7 @@ class Site extends MX_Controller
     
         }
 
-        // public function view_single($school_id)
-        public function view_single()
-    
-        {
-            // echo  $school_id;die();
-            //$v_data['abouts'] = $this->sites_model->get_about_posts();
-            $v_data['get_donors'] = $this->sites_model->get_donations();
-            $v_data['get_dignity_packs'] = $this->sites_model->get_donations();
-            $donations = $this->sites_model->get_donation_totals();
-            $v_data['news_items'] = $this->sites_model->get_new_items();
-            $v_data['pictures'] = $this->sites_model->get_gallery_pictures();
-            $v_data['schools'] = $this->sites_model->get_schools();
-            $v_data['partners'] = $this->sites_model->get_partners();
-            $v_data['allschools'] = $this->sites_model->get_all_schools();
-            //echo json_encode($v_data['allschools']->result());die();
-            
-            $project_donation_total = $project_target_total = $percentage_donated_total = 0;
-            if ($donations->num_rows() > 0) {
-                foreach ($donations->result() as $row) {
-                    
-                    $project_donation_total = $row->total_donated_amount;
-                    $project_target_total = $row->total_target_amount;
-                    $percentage_donated_total = round(($project_donation_total / $project_target_total) * 100);
-                    
-                }
-            }
-            $v_data['project_donation_total'] = $project_donation_total;
-            $v_data['project_target_total'] = $project_target_total;
-            $v_data['percentage_donated_total'] = $percentage_donated_total;
-            $data['content'] = $this->load->view('site/home/school_single', $v_data, true);
-            $data['title'] = $this->sites_model->display_page_title();
-            $this->load->view("site/layouts/layout", $data);
-    
-        }
 
-        
         public function read_more($school_id)
         {
 
