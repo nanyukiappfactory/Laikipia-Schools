088f77acc63a2bba280a3538d4c07f42aa2e50b7
Muraura <patricia.kanana@nanyukiappfactory.onmicrosoft.com>
2019-03-13 12:24:52 +0300

    divs in all schools

--- application/modules/laikipiaschools/models/Site_model.php
+++ application/modules/laikipiaschools/models/Site_model.php
@@ -26,16 +26,9 @@ class Site_model extends CI_Model
         return $field_name;
     }
 
-    // public function get_all_categories()
-    // {
-    //     $this->db->select('category.*, post.post_id, post.post_title');
-    //     $this->db->from('category');
-    //     $this->db->join('post', 'post.category_id=category.category_id', 'left');
-    //     $this->db->group_by('category.category_id');
-    //     return $this->db->get();
-    // }
+    
 
-    public function get_categories()
+    public function get_all_categories()
     {
 
         $this->db->select('category.category_id,category.category_name, post.post_id, post.post_title');
