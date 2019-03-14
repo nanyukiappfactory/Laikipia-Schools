16aeee67a2bab43e1e1bb0a708c55f23edd1090f
Muraura <patricia.kanana@nanyukiappfactory.onmicrosoft.com>
2019-03-14 15:09:54 +0300

    pagination working

--- application/modules/laikipiaschools/models/Site_model.php
+++ application/modules/laikipiaschools/models/Site_model.php
@@ -31,7 +31,8 @@ class Site_model extends CI_Model
         $this->db->select('category.*, post.post_id, post.post_title');
         $this->db->from('category');
         $this->db->join('post', 'post.category_id=category.category_id', 'left');
-        // $this->db->group_by('category.category_id');
+         $this->db->where('category.deleted=0');
+         $this->db->where('post.deleted=0');
         $query =  $this->db->get();
         //echo json_encode($query->result());die();
         return $query;
