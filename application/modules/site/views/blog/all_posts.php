9f8c7011436209d4d457325555e0d7e4ed21b2a2
alvie <alvaromasitsa104@gmail.com>
2019-03-18 17:01:29 +0300

    Thumbnails in blog fixed

--- application/modules/site/views/blog/all_posts.php
+++ application/modules/site/views/blog/all_posts.php
@@ -17,8 +17,7 @@
 				<ul class="list-inline">
 					<li><a href="<?php echo site_url();?>"> Home </a></li>
 					<li><a href="#"> <i class="fas fa-angle-double-right"></i> </a></li>
-					<li><a href="<?php echo site_url();?>blog"> Blog </a></li>
-					<li><a href="#"> Mobilies UX Trend </a></li>
+					<li><a href="#"> Blog </a></li>
 				</ul>
 			</div>
 		</section>
